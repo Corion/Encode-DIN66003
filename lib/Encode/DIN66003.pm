27c11f6ea71671a755c5c27dff3c79a20aae23e8
Max Maischein <corion@corion.net>
2017-01-23 19:55:35 +0100

    Strip whitespace

--- lib/Encode/DIN66003.pm
+++ lib/Encode/DIN66003.pm
@@ -1,7 +1,7 @@
 package Encode::DIN66003;
 use vars qw($VERSION);
 $VERSION = "0.02";
- 
+
 use Encode;
 use XSLoader;
 XSLoader::load(__PACKAGE__,$VERSION);
@@ -12,13 +12,13 @@ __END__
 =encoding UTF-8
 
 =head1 NAME
- 
+
 Encode::DIN66003 - Encoding according to DIN 66003
- 
+
 =head1 SYNOPSIS
 
   use Encode::DIN66003;
-  
+
   # If your terminal is UTF-8
   print decode('DIN66003', 'Hello W|rld!'); # Hello Wörld
 
